0e83ec6981fe847b2b5f7750b22201e9cb4bde85
stvwid2 <playoffperis@gmail.com>
2026-04-20 10:55:15 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE='escala7bocas-v1';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp
+}).catch(()=>caches.match('./index.html'))))
+});
